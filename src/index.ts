445c19eb761e4e3787d7fe026ad0cf5bfbbe1fde
rhea-so <rhea31@naver.com>
2020-12-16 23:36:40 +0900

    update: service 장애 시나리오 테스트 1

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,14 @@ import * as amqp from 'amqplib/callback_api';
 
 
 class AMQPClient {
+  private serverAddress: string;
   private username: string;
   private password: string;
   private connection: any;
   private channel: any;
   
-  public async init(username: string, password: string): Promise<void> {
+  public async init(serverAddress: string, username: string, password: string): Promise<void> {
+    this.serverAddress = serverAddress;
     this.username = username;
     this.password = password;
     
@@ -24,10 +26,23 @@ class AMQPClient {
   }
   
   public async sendMessage(queue: string, message: string): Promise<void> {
-        this.channel.assertQueue(queue, {
-            durable: false
-        });
-        this.channel.sendToQueue(queue, Buffer.from(message));
+    this.channel.assertQueue(queue, {
+        durable: true // RabbitMQ가 꺼져도, Queue가 제거되지 않고 남아있게 함
+    });
+    this.channel.sendToQueue(queue, Buffer.from(message), {
+      persistent: true // RabbitMQ가 꺼져도, Queue에 데이터가 제거되지 않고 남아있게 함
+    });
+  }
+  
+  public receiveMessage(queue: string, func: (message: string) => Promise<void> | void, maxCount: number = 1): void {
+    this.channel.assertQueue(queue, {
+        durable: true // RabbitMQ가 꺼져도, Queue가 제거되지 않고 남아있게 함
+    });
+    this.channel.prefetch(maxCount); // 최대로 수용할 수 있는 메세지 수
+    this.channel.consume(queue, async (message) => {
+      await func(message?.content?.toString());
+      this.channel.ack(message); // ack를 보내기 전에 서버가 죽으면, RabbitMQ가 해당 메세지를 자동으로 re-queue 한다. - https://blurblah.net/1569
+    });
   }
   
   public async free(): Promise<void> {
@@ -38,7 +53,7 @@ class AMQPClient {
   
   private async connect(): Promise<void> {
     return new Promise((resolve, reject) => {
-      amqp.connect(`amqp://${this.username}:${this.password}@193.123.240.173`, (error, connection) => {
+      amqp.connect(`amqp://${this.username}:${this.password}@${this.serverAddress}`, (error, connection) => {
         if (error) {
           Debug.log(LogTag.ERROR, 'CAN_NOT_CONNECT_TO_SERVER', error);
           reject(error);
@@ -64,15 +79,46 @@ class AMQPClient {
 }
 
 
+
+
 async function main() {
-   const client = new AMQPClient();
-   await client.init(process.env.USERNAME as string, process.env.PASSWORD as string);
-   await client.sendMessage('hello', 'hello, world!');
-    setTimeout(() => {
-        Debug.log('Connection End');
-        client.free();
-        process.exit(0);
-    }, 5000);
+  const client1 = new AMQPClient();
+  await client1.init(process.env.SERVER_ADDRESS as string, process.env.USERNAME as string, process.env.PASSWORD as string);
+  
+  const client2 = new AMQPClient();
+  await client2.init(process.env.SERVER_ADDRESS as string, process.env.USERNAME as string, process.env.PASSWORD as string);
+  
+  const client3 = new AMQPClient();
+  await client3.init(process.env.SERVER_ADDRESS as string, process.env.USERNAME as string, process.env.PASSWORD as string);
+  
+  client1.receiveMessage('hello', async (message) => {
+    Debug.log('CLIENT1', message);
+  });
+  client2.receiveMessage('hello', async (message) => {
+    Debug.log('CLIENT2', message);
+  }, 4);
+  client3.receiveMessage('hello', async (message) => {
+    Debug.log('CLIENT3', message);
+  });
+  
+  client1.sendMessage('hello', 'nice to meet you 1');
+  client1.sendMessage('hello', 'nice to meet you 2');
+  client1.sendMessage('hello', 'nice to meet you 3');
+  client1.sendMessage('hello', 'nice to meet you 4');
+  client1.sendMessage('hello', 'nice to meet you 5');
+  client1.sendMessage('hello', 'nice to meet you 6');
+  client1.sendMessage('hello', 'nice to meet you 7');
+  client1.sendMessage('hello', 'nice to meet you 8');
+  client2.sendMessage('hello', 'nice to meet you 9');
+  client3.sendMessage('hello', 'nice to meet you 10');
+  
+  setTimeout(() => {
+    Debug.log('Connection End');
+    client1.free();
+    client2.free();
+    client3.free();
+    process.exit(0);
+  }, 500);
 }
 
 main();
